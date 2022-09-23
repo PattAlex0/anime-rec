8e024cdecab7da595af6f19a3e4550ef81b21b03
Alex Patterson <alexthepatterson@gmail.com>
2022-09-23 13:32:44 +0100

    Added argument to allow for parsing choice

--- read_page.py
+++ read_page.py
@@ -1,8 +1,9 @@
-def read_page(url):
+def read_page(url, parse = 'html.parser'):
     """Scrapes the content from a URL. Taken from Practical Data Science (https://practicaldatascience.co.uk/data-science/how-to-scrape-and-parse-a-robotstxt-with-python).
 
     Args:
         url (string): The URL you want to scrape
+        parse (string): The content of the url that you want to parse. Function imports lxml to allow for xml parsing
 
     Returns:
         soup (string): HTML source
@@ -11,6 +12,7 @@ def read_page(url):
     # Import libraries
     from urllib.request import urlopen, Request
     from bs4 import BeautifulSoup
+    import lxml
 
     # Request url
     req = Request(url)
@@ -19,7 +21,7 @@ def read_page(url):
     opn = urlopen(req)
 
     # Get data
-    soup = BeautifulSoup(opn, 'html.parser')
+    soup = BeautifulSoup(opn, parse)
 
     # Return data
     return soup
